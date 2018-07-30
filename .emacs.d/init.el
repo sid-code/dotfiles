1bc71fec46f3cb38f77becf13b8398820b2c2752
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2018-07-30 12:17:21 -0700

    Smart tabs configuration

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -246,10 +246,18 @@ The default value for this parameter is in the variable `default-terminal-name'.
   :defer t
   :init
   (setq-default indent-tabs-mode nil)
-  (smart-tabs-insinuate 'c 'c++)
+  (smart-tabs-insinuate 'c 'c++ 'java)
+  (smart-tabs-mode)
 
   :config
-  (add-hook 'c-mode-common-hook (lambda () (setq indent-tabs-mode t))))
+  (add-hook 'java-mode-hook (lambda ()
+                              (setq c-basic-offset 4
+                                    tab-width 4
+                                    indent-tabs-mode t)))
+  (add-hook 'c-mode-common-hook (lambda ()
+                                  (setq c-basic-offset 8
+                                        tab-width 8
+                                        indent-tabs-mode t))))
 
 (use-package pc-bufsw
   :defer t
