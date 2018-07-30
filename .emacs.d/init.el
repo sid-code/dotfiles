924efe5a56c989abd12311d4a2195f3c00428a8e
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2018-07-30 12:17:42 -0700

    Comment out nimsuggest stuff because it's broken

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -384,10 +384,10 @@ The default value for this parameter is in the variable `default-terminal-name'.
   :ensure t
   :defer t
   :init
-  (setq nimsuggest-path "/home/sid/bin/nimsuggest")
-  :hook ((nim-mode-hook . nimsuggest-mode)
-         (nimsuggest-mode-hook . company-mode)
-         (nimsuggest-mode-hook . flycheck-mode)))
+  (setq nimsuggest-path (concat sid/homedir "-NOPE/bin/nimsuggest")))
+  ;:hook ((nim-mode-hook . nimsuggest-mode)
+  ;       (nimsuggest-mode-hook . company-mode)
+  ;       (nimsuggest-mode-hook . flycheck-mode)))
 
 (use-package cquery
   :ensure t
