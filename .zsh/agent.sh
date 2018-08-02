ac9b725e25dd91d85e3feaf93e6071a228af8497
sid-code <kulkarnisidharth1@gmail.com>
2018-08-01 20:11:55 -0700

    symlink /tmp/ssh-QZx1p9WR5pqQ/agent.615 to /home/sid/ssh_auth_sock

--- .zsh/agent.sh
+++ .zsh/agent.sh
@@ -1,5 +1,7 @@
 if ! which keychain 2>&1 >/dev/null ; then
     echo "no keychain!"
 else
+    rm /home/sid/ssh_auth_sock
     eval `keychain --quiet --eval id_rsa`
+    ln -s $SSH_AUTH_SOCK /home/sid/ssh_auth_sock
 fi
