fc2cec2565968d1b8d10862e7241f0040182c823
sid-code <kulkarnisidharth1@gmail.com>
2018-09-13 10:56:44 -0700

    Add class selection mechanism

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -47,6 +47,27 @@
   (setq display-time-format "%H:%M - %d %B %Y")
   (display-time-mode t))
 
+;; Quick class navigation
+(defvar sid/school-path (concat sid/homedir "/asu") "School files base directory.")
+(defun sid/get-class-alist (base-path)
+  "Gets an alist of class descriptions and their directories.
+Use BASE-PATH as the base path."
+  (map 'list
+       (lambda (x)
+         (cons (with-temp-buffer
+                 (insert-file-contents x)
+                 (buffer-string))
+               (file-name-directory x)))
+       (file-expand-wildcards (concat (file-name-as-directory base-path) "*/.metadata"))))
+
+(defun sid/find-class ()
+  "Open a menu to select a class."
+  (interactive)
+  (ivy-read "Select a class: " (sid/get-class-alist sid/school-path)
+            :action (lambda (pair)
+                      (find-file (cdr pair)))))
+
+
 ;; Release C-c in term-- use C-x as prefix key
 (eval-after-load "term"
   '(progn (term-set-escape-char ?\C-x)
