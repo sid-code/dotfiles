ac900adbb4e4af35ba2eef4639fb8d4b08c3faad
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2018-09-27 13:58:56 -0700

    Remove old google-this use-package comment

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -491,12 +491,6 @@ The default value for this parameter is in the variable `default-terminal-name'.
   :init
   (add-hook 'lsp-mode-hook 'lsp-ui-mode))
   
-;;(use-package google-this
-;;  :ensure t
-;;  :defer t
-;;  :init
-;;  (google-this-mode 1))
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
