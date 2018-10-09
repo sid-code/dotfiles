ee4a3ba6b32fceba2ec908f83d4fe749bf3a1587
sid-code <kulkarnisidharth1@gmail.com>
2018-10-08 19:55:56 -0700

    Check emacs version before package-initialize

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -5,11 +5,9 @@
 
 ;; load essential packages
 
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-(package-initialize)
+(if (version< emacs-version "27.0")
+    (package-initialize)
+  'package-initialization-skipped)
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
