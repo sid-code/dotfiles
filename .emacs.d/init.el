ea28b81d2b5459cf2762c1b279b2bce84b26416b
sid-code <kulkarnisidharth1@gmail.com>
2018-10-08 19:58:47 -0700

    Use a custom.el for custom-set-stuff

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -548,26 +548,6 @@ The default value for this parameter is in the variable `default-terminal-name'.
 ;  :init
 ;  (rcirc-track-minor-mode))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
- '(doc-view-continuous t)
- '(org-agenda-files (quote ("~/sync/org/school.org")))
- '(org-babel-load-languages (quote ((C . t) (emacs-lisp . t))))
- '(package-selected-packages
-   (quote
-    (gnuplot gnuplot-mode which-key counsel-projectile dmenu cquery-mode cquery lsp-mode google-this dix-evil pdf-tools nim-mode epa-file org-mode org-journal emms w3m simpleclip webpaste web-mode matlab-mode all-the-icons-dired neotree doom-themes company tide csv-mode use-package flycheck volume pulseaudio-control markdown-mode ess polymode smart-mode-line auctex pc-bufsw multi-term magit evil-magit smart-tabs-mode switch-window exwm projectile helm go-mode nlinum evil)))
- '(reb-re-syntax (quote string))
- '(select-enable-clipboard nil)
- '(tramp-default-method "ssh"))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; custom stuff
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
