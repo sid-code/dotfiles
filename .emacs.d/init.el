57211378723b341db808fd5398dac736dee98b26
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2018-10-23 23:08:58 -0700

    Add avy config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -203,6 +203,21 @@ The default value for this parameter is in the variable `default-terminal-name'.
   :bind (:map pdf-view-mode-map ("j" . pdf-view-next-line-or-next-page)
          :map pdf-view-mode-map ("k" . pdf-view-previous-line-or-previous-page)))
 
+(use-package avy
+  :ensure t
+  :defer t
+
+  :config
+  (evil-define-key 'normal 'global "S" nil)
+  (evil-define-key 'normal 'global "SA" 'avy-goto-char)
+  (evil-define-key 'normal 'global "SS" 'avy-goto-char-2)
+  (evil-define-key 'normal 'global "SD" 'avy-goto-char-timer)
+  (evil-define-key 'normal 'global "Sw" 'avy-goto-word-1)
+  (evil-define-key 'normal 'global "SW" 'avy-goto-word-0)
+  (evil-define-key 'normal 'global "SL" 'avy-goto-line)
+  :bind (("C-:" . avy-goto-char)
+         ("C-;" . avy-goto-char-2)))
+
 (use-package ivy
   :ensure t
   :init
