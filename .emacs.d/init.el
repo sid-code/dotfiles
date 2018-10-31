2a5beb1beb7ad32cd194f55b3aed747d35e1c8fe
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2018-10-30 20:43:13 -0700

    Clean out exwm-specific config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -77,13 +77,13 @@
 (server-start)
 
 ;; exwm
-(load-file "~/.emacs.d/exwm-init.el")
+;(load-file "~/.emacs.d/exwm-init.el")
+;; exwm-input-set-key shim
+(defun exwm-input-set-key (key fun)
+  (global-set-key key fun))
 
 ;; keybinds
 (progn
-  (exwm-input-set-key (kbd "s-w") 'exwm-workspace-switch)
-  (exwm-input-set-key (kbd "s-m") 'exwm-workspace-move-window)
-  (exwm-input-set-key (kbd "s-s") 'exwm-workspace-swap)
   (exwm-input-set-key (kbd "s-a") 'org-agenda)
 
   (exwm-input-set-key (kbd "s-z") 'winner-undo)
@@ -95,55 +95,8 @@
   (global-set-key (kbd "C-x C-r") 'rename-buffer)
   (exwm-input-set-key (kbd "C-x M-f") 'sid/find-class)
 
-  (defvar sid/exwm-last-workspace exwm-workspace--current "The last active EXWM workspace.")
-  (defun sid/exwm-record-last-workspace (orig-fn &rest args)
-    (setq sid/exwm-last-workspace exwm-workspace--current)
-    (apply orig-fn args))
-
-  (defun sid/exwm-workspace-switch-last () (interactive)
-    (exwm-workspace-switch sid/exwm-last-workspace))
-
-  (advice-add 'exwm-workspace-switch :around #'sid/exwm-record-last-workspace)
-
-  (exwm-input-set-key (kbd "s-<tab>") 'sid/exwm-workspace-switch-last)
-
-  (exwm-input-set-key (kbd "s-`") (lambda () (interactive) (exwm-workspace-switch 0)))
-  (dotimes (i 10)
-    (exwm-input-set-key (kbd (format "s-%d" i))
-                        `(lambda ()
-                           (interactive)
-                           (exwm-workspace-switch-create ,i))))
-
-  (exwm-input-set-key (kbd "s-R") (lambda () (interactive) (call-interactively #'exwm-reset)))
-  (exwm-input-set-key (kbd "s-r") (lambda (command) (interactive (list (read-shell-command "$ ")))
-                                    (start-process-shell-command command nil command)))
-  (exwm-input-set-key (kbd "s-o")
-                      (lambda () (interactive) (start-process "" nil "/usr/bin/slock")))
-  (exwm-input-set-key (kbd "s-<return>")
-                      (lambda () (interactive) (start-process "" nil "/usr/bin/urxvt")))
-  (exwm-input-set-key (kbd "s-c")
-                      (lambda() (interactive) (start-process "" nil "/home/sid/bin/qute-launch")))
 
   (exwm-input-set-key (kbd "s-C") #'kill-buffer-and-window)
-
-  (exwm-input-set-key (kbd "s-i")
-                      (lambda () (interactive) (call-interactively #'exwm-input-release-keyboard)))
-  (exwm-input-set-key (kbd "s-<escape>")
-                      (lambda () (interactive) (call-interactively #'exwm-input-grab-keyboard)))
-
-  (exwm-input-set-key (kbd "s-<f2>")
-                      (lambda () (interactive)
-                        (shell-command "/home/sid/code/dwmbar/dwmstatus/rawstatus --once")))
-
-  (defun sid/multi-term-dedicated-toggle-smart () (interactive)
-         (progn
-           (call-interactively 'multi-term-dedicated-toggle)
-           (if (window-valid-p multi-term-dedicated-window)
-               (call-interactively 'multi-term-dedicated-select))))
-
-  (exwm-input-set-key (kbd "<f5>") 'sid/multi-term-dedicated-toggle-smart)
-  (global-set-key (kbd "s-<f5>") 'sid/multi-term-dedicated-toggle-smart)
-  (exwm-input-set-key (kbd "<f6>") 'multi-term-dedicated-select)
 
   (exwm-input-set-key (kbd "s-q") 'delete-window)
   (exwm-input-set-key (kbd "s-h") 'windmove-left)
@@ -159,8 +112,6 @@
                                     (call-interactively #'windmove-down)))
   (exwm-input-set-key (kbd "s-H") 'split-window-right)
   (exwm-input-set-key (kbd "s-K") 'split-window-below)
-  (exwm-input-set-key (kbd "s-x") (lambda () (interactive)
-                                    (call-interactively #'exwm-floating-toggle-floating)))
 
   (exwm-input-set-key (kbd "<pause>") (lambda () (interactive)
                                         (start-process "" nil "/home/sid/sync/bin/unipause")))
