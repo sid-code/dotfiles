d137aa9458f87ea692c3c3cdd8d1af45702c23f8
sid-code <kulkarnisidharth1@gmail.com>
2019-05-08 16:49:09 -0700

    Remove old nim path entry

--- .zsh/40-vars.sh
+++ .zsh/40-vars.sh
@@ -7,7 +7,6 @@ export NPM_PACKAGES="$HOME/.npm-packages"
 # Add new PATH entries here
 export PATH="$PATH:/usr/local/bin"
 export PATH="$PATH:$HOME/.local/bin" # pip and perhaps other things
-export PATH="$PATH:$HOME/Nim/bin"
 export PATH="$PATH:$HOME/.nimble/bin"
 export PATH="$PATH:$HOME/bin"
 export PATH="$PATH:$HOME/sync/bin"
