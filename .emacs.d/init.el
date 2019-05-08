6cd77f3d644c1c5ede3dc94b3bb719ccf83a27b8
sid-code <kulkarnisidharth1@gmail.com>
2019-05-08 16:47:43 -0700

    Update nimsuggest path

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -564,7 +564,7 @@ The default value for this parameter is in the variable `default-terminal-name'.
   :ensure t
   :defer t
   :init
-  (setq nimsuggest-path "/home/sid/Nim/bin/nimsuggest")
+  (setq nimsuggest-path "/home/sid/.nimble/bin/nimsuggest")
   :hook ((nim-mode . (lambda () (setq evil-shift-width 2)))
          (nim-mode . nimsuggest-mode)
          (nimsuggest-mode . company-mode)
