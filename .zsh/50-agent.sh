287db30c7d4b271279b569735046498e11c3d813
sid-code <kulkarnisidharth1@gmail.com>
2019-08-28 14:39:37 -0700

    Add other key to keychain [doa only]

--- .zsh/50-agent.sh
+++ .zsh/50-agent.sh
@@ -2,6 +2,6 @@ if ! which keychain 2>&1 >/dev/null ; then
     echo "no keychain!"
 else
     rm -r $HOME/ssh_auth_sock
-    eval `keychain --quiet --eval id_rsa`
+    eval `keychain --quiet --eval id_rsa id_ecdsa`
     ln -s $SSH_AUTH_SOCK $HOME/ssh_auth_sock
 fi
